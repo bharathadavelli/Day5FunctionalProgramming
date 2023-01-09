8349acfa42bde3081cb91eb162909c8968a5aea2
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-09 14:43:27 +0530

    [Bharath]-Leapyear

---
+++ Day5BasicFunctionalProgramming/Day5BasicFunctionalProgramming/Leapyear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5BasicFunctionalProgramming
+{
+    internal class Leapyear
+    {
+        public static void IsLeapYear()
+        {
+            Console.WriteLine("Enter a four digit year : ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine(year  + "It is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year  + "It is not a leap year");
+            }
+            }
+        }
+    }
--- Day5BasicFunctionalProgramming/Day5BasicFunctionalProgramming/Program.cs
+++ Day5BasicFunctionalProgramming/Day5BasicFunctionalProgramming/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Welcome to Day5 Basic and Functional Programming");
-            Flipcoin.HeadsorTails();
+            //Flipcoin.HeadsorTails();
+            Leapyear.IsLeapYear();
         }
     }
 }
