c7e8ad98a298055aadd44780f953cfc8c8c1b93b
“bharathadavelli” <bharathadavelli@gmail.com>
2023-01-09 13:23:48 +0530

    [Bharath]-Welcome msg

---
+++ Day5BasicFunctionalProgramming/Day5BasicFunctionalProgramming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day5BasicFunctionalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day5 Basic and Functional Programming");
+        }
+    }
+}
